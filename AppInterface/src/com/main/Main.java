311a7ee68583c95bf9f7c127e3bf4707a6c1f8d4
alexbuntaran <alexbuntaran@kingsstudent.org>
2020-05-08 14:31:49 -0700

    updated initial commit

--- AppInterface/src/com/main/Main.java
+++ AppInterface/src/com/main/Main.java
@@ -3,7 +3,8 @@ package com.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("C.E.L.E.R.Y");
+        Interface gui = Interface.getInstance();
+        gui.initialize();
 	}
 	
 }
